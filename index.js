8f329c0ef192c7b096bc8652514c58f624f84b1c
vimal <selvamvimaldz1@gmail.com>
2023-10-19 07:10:08 +0530

    engine add in package

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import route from './src/route/index.js';
 const app=express();
 app.use(express.json());
 app.use('/',route)
-app.listen(8001,()=>{console.log(`port listen into ${8001}`);})
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{console.log(`port listen into ${PORT}`);})
 
